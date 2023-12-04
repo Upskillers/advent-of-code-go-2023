6fd13f5fb7a822e07a87ec552fcf87253594e678
Katie Maison <kmm565@york.ac.uk>
2023-12-04 19:41:40 +0000

    Adjust day 1

--- day-1/day-1.go
+++ day-1/day-1.go
@@ -10,17 +10,19 @@ import (
 func main() {
 	lines, _ := util.ReadFileLineByLine("./day-1/input.txt")
 	sum := 0
-	nums := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-	re := regexp.MustCompile(`(zero|one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9|0)`)
-
+	nums := []string{"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	re := regexp.MustCompile(`(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)`)
 	for _, line := range lines {
+		fmt.Println(line)
 		matches := re.FindAllString(line, -1)
+		if matches == nil {
+			fmt.Println("No matches found")
+		}
 		fmt.Println(matches)
 		for x, match := range matches {
 			for i, num := range nums {
 				if match == num {
-					fmt.Println(i)
-					matches[x] = string(i)
+					matches[x] = strconv.Itoa(i)
 				}
 			}
 		}
@@ -31,6 +33,8 @@ func main() {
 		if err == nil {
 			sum += intValue
 		}
+		fmt.Println(intValue)
+		fmt.Println(sum)
 
 	}
 	fmt.Println(sum)
